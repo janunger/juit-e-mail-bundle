6539749dd08b71b2da0539387dd2579ba8a4d34c
Jan Unger <ju@juit.de>
2014-03-22 18:56:10 +0100

    Having an abstract command class that sends spooled emails at the end of the execution

---
+++ src/JUIT/EMailBundle/Command/MailSendingCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace JUIT\EMailBundle\Command;
+
+use Swift_FileSpool;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+abstract class MailSendingCommand extends ContainerAwareCommand
+{
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    public function run(InputInterface $input, OutputInterface $output)
+    {
+        $result = parent::run($input, $output);
+
+        $this->sendQueuedMails();
+
+        return $result;
+    }
+
+    protected function sendQueuedMails()
+    {
+        /** @var $transport \Swift_Transport */
+        $transport = $this->getContainer()->get('swiftmailer.transport.real');
+
+        /** @var $spool Swift_FileSpool */
+        $spool = $this->getContainer()->get('mailer')->getTransport()->getSpool();
+
+        $spool->flushQueue($transport);
+    }
+}
